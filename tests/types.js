10fdc89cf2d531a77bce3a0435581c248005bc1b
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-08-12 12:48:07 -0700

    Added Tests for Correct Return Types

---
+++ tests/types.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const { expect } = require("chai");
+const TotallyRandom = require("../dist/index.js");
+
+const random = new TotallyRandom();
+
+describe("TotallyRandom", () => {
+    describe("#boolean", () => {
+        it("should return either true or false", () => {
+            expect(random.boolean()).to.be.oneOf([true, false])
+        })
+    })
+})
