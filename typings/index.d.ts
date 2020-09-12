c8363b5dba29a1ab9eb2eddb5d3f2bc3fdfbf1a5
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-12 14:10:20 -0700

    Format Script, Formatted Code

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,29 +1,38 @@
 export = TotallyRandom;
 
 declare class TotallyRandom {
-    constructor(randomizer?: () => number);
+  constructor(randomizer?: () => number);
 
-    randomizer: () => number;
+  randomizer: () => number;
 
-    array<T>(arr: T[], count?: number, unique?: boolean): (T | T[]);
+  array<T>(arr: T[], count?: number, unique?: boolean): T | T[];
 
-    between(num1: number, num2: number, count?: number): (number | number[]);
+  between(num1: number, num2: number, count?: number): number | number[];
 
-    boolean(count?: number): (boolean | boolean[]);
+  boolean(count?: number): boolean | boolean[];
 
-    chance(arr: any[], count?: number): any;
+  chance(arr: any[], count?: number): any;
 
-    color(option?: ("hex" | "rgb" | "rgba" | "hsl" | "hsla"), count?: number): (string | string[]);
+  color(
+    option?: "hex" | "rgb" | "rgba" | "hsl" | "hsla",
+    count?: number
+  ): string | string[];
 
-    floatTo(num: number, count?: number): (number | number[]);
+  floatTo(num: number, count?: number): number | number[];
 
-    percent(count?: number): (number | number[]);
+  percent(count?: number): number | number[];
 
-    shuffle<T>(arr: T[]): T[];
+  shuffle<T>(arr: T[]): T[];
 
-    string(option?: ("alphanumeric" | "alpha" | "numeric"), length?: number): string;
+  string(
+    option?: "alphanumeric" | "alpha" | "numeric",
+    length?: number
+  ): string;
 
-    to(num: number, count?: number): (number | number[]);
+  to(num: number, count?: number): number | number[];
 
-    colour(option?: ("hex" | "rgb" | "rgba" | "hsl" | "hsla"), count?: number): (string | string[]);
+  colour(
+    option?: "hex" | "rgb" | "rgba" | "hsl" | "hsla",
+    count?: number
+  ): string | string[];
 }
